a88b263e042a9b9ec1a485fb9a23c55d01367165
skywind3000 <skywind3000@2c106b56-4793-4ec6-2fc1-3cec70d20447>
2010-12-12 12:26:52 +0000

    commit new math operation

--- trunk/ibmwink.h
+++ trunk/ibmwink.h
@@ -10,44 +10,49 @@
 #ifndef __IBMWINK_H__
 #define __IBMWINK_H__
 
+#include <stddef.h>
+#include <math.h>
+
 #include "ibitmap.h"
 #include "ibitmapm.h"
 #include "ibmcols.h"
 #include "ibmfast.h"
 
+
 //---------------------------------------------------------------------
-// Rectangle
+// 几何结构
 //---------------------------------------------------------------------
-/* clip area */
-#define _ibitmap_set_clip(bmp, ptr) (bmp)->code = (unsigned long)(ptr)
-#define _ibitmap_clip(bmp) ((int*)((bmp)->code))
+struct IPOINT2D
+{
+	float x;
+	float y;
+};
+
+struct IRECT2D
+{
+	float x0;
+	float y0;
+	float x1;
+	float y1;
+};
+
+typedef struct IRECT2D IRECT2D;
+typedef struct IPOINT2D IPOINT2D;
+typedef IPOINT2D IVECTOR2D;
+
+
+//---------------------------------------------------------------------
+// 浮点数操作
+//---------------------------------------------------------------------
+
+
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 //---------------------------------------------------------------------
 // Simple Drawing
-//---------------------------------------------------------------------
-
-void ibitmap_clip_create(IBITMAP *bmp, int cl, int ct, int cr, int cb);
-
-void ibitmap_clip_delete(IBITMAP *bmp);
-
-void ibitmap_setpixel(IBITMAP *bmp, int x, int y, ICOLORD rgb);
-
-ICOLORD ibitmap_getpixel(const IBITMAP *bmp, int x, int y);
-
-void ibitmap_line(IBITMAP *bmp, int x1, int y1, int x2, int y2, ICOLORD c);
-
-void ibitmap_circle(IBITMAP *bmp, int x, int y, int r, ICOLORD c, int fill);
-
-void ibitmap_rect(IBITMAP *bmp, int x, int y, int w, int h, ICOLORD c);
-
-void ibitmap_box(IBITMAP *bmp, int x, int y, int w, int h, ICOLORD c);
-
-
-//---------------------------------------------------------------------
-// Bitmap Transform
 //---------------------------------------------------------------------
 
 
